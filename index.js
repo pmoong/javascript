b7d76c879b7775d60150102dbdbd2641f9a3de2e
pmoong <cornpu@naver.com>
2026-09-15 10:53:34 +0900

    javascript

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 console.log("Hello js")
 
 const username = "student";
-console.log(username)
+console.log("username",username)
+
+const num = 10;
+console.log("✏️ ~ num:", num, "num type", typeof num)
+
+const text = "안녕하세요";
+console.log("✏️ ~ text:", text, "text type", typeof text)
+
+const isDarkMode = true;
+console.log("✏️ ~ isDarkMode:", isDarkMode, "isDarkMode type", typeof isDarkMode)
+
+const empty = null;
+console.log("✏️ ~ empty:", empty, "empty type", typeof empty)
